672dc8c8b7f5885119620b8092c034552d7ca00f
mist <74228226+Mist10148@users.noreply.github.com>
2026-09-10 01:04:28 +0800

    Enhance game logic to track wins and losses, and improve feedback on attempts in Number Guessing Game

--- NumberGuessing.java
+++ NumberGuessing.java
@@ -6,6 +6,10 @@ public class NumberGuessing {
         Scanner input = new Scanner(System.in);
         Random rd = new Random();
 
+        //variables
+        int totalWins = 0;
+        int totalLosses = 0;
+
 
 
         //welcome message and instructions for the user
@@ -41,28 +45,47 @@ public class NumberGuessing {
                 }
 
                 else{
-                    System.out.println("Congratulations! You guessed the number " + number + " in " + attempts + " attempts.");
+                    System.out.println("Congratulations! You guessed the number");
+
+
                 }
+
+                //conditionals to check the number of attempts left
                 if(attempts >= 5 && guess != number){
                     System.out.println("You have used all your attempts. The number was " + number);
                 }
 
-                //conditionals to check the number of attempts left
-                if(attempts < 5 && guess != number){
+                else if(attempts < 5 && guess != number){
                     System.out.println("You have " + (5 - attempts) + " attempts left.");
                 }
-                else if(attempts == 5 && guess != number){
-                    System.out.println("You have no attempts left.");
-                    break;
-                }
-
+           
         }while(guess != number && attempts < 5);
 
+        //round results displaying the win or lose statuses and the number of attempts used
+        if(guess == number){
+            System.out.println("Round: " + i + "WIN - " + attempts + " attempts");
+            totalWins++;
+        }
 
-
+        else{
+            System.out.println("Round: " + i + "LOSE - ");
+            totalLosses++;
+        }
 
 
     }
+
+    //display of statistics after thee completion of the game
+
+    //display of overall game results(per round) on whether the user won or lost and the number of attempts used
+
+    System.out.println("Total Wins: " + totalWins); //total wins
+    System.out.println("Total losses: " + totalLosses); //total losses
+    System.out.println("Average attempts per round: " + (double)(totalWins + totalLosses) / 5); //Average attempts
+    System.out.println("Final Score: " + (totalWins * 20)); //final score up to 100
+
+
+    
 
     input.close();
 }
