cda97b616296c7114b6c06ef6cb7c1877961bb57
mist <74228226+Mist10148@users.noreply.github.com>
2026-09-10 00:08:07 +0800

    Implement Number Guessing Game logic with user input and feedback

--- NumberGuessing.java
+++ NumberGuessing.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+import java.util.Random;
 public class NumberGuessing {
     public static void main(String[] args){
-        
+        //making an object
+        Scanner input = new Scanner(System.in);
+        Random rd = new Random();
+
+        //variable declarations
+        int number = rd.nextInt(100) + 1;
+        int guess = 0;
+        int attempts = 0;
+
+
+        do{
+            //welcome message and ask the user for input
+            System.out.println("Welcome to the Number Guessing Game!");
+            System.out.println("I have selected a number between 1 and 100. Try to guess it!");
+            System.out.print("Enter your guess: ");
+            guess = input.nextInt();
+            attempts++;
+
+
+            //conditional checking for the player's guess
+            if(guess < number) {
+                System.out.println("Your guess is too low! Try again.");
+            }
+
+            else if(guess > number){
+                System.out.println("Your guess is too high! Try again.");
+            }
+
+            else{
+                System.out.println("Congratulations! You've guessed the number " + number + " in " + attempts + " attempts.");
+
+            }
+
+        }while(guess != number);
+
+        input.close();
+
     }
 }
