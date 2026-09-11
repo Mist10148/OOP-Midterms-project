9ef3b24798ec399b72ed03aea61475b21c05f75d
mist <74228226+Mist10148@users.noreply.github.com>
2026-09-11 18:00:29 +0800

    Refactor input handling and variable declarations in NumberAnalysis class

--- NumberAnalysis.java
+++ NumberAnalysis.java
@@ -5,14 +5,27 @@ public class NumberAnalysis {
         //making an object scanner for input
         Scanner input = new Scanner(System.in);
 
-
+        //welcome message and getting input from the user
         System.out.print("Enter a starting number:");
         int start = input.nextInt();
 
         System.out.print("Enter an ending number: ");
         int end = input.nextInt();
 
+        //main variable declaration
+        int primeCount = 0, compositeCount = 0, evenCount = 0, oddCount = 0, divisorCount = 0;
 
+        for(int i = start; i <= end; i++){
+
+            //condition in counting the odd and even numbers
+            if(i % 2 == 0){
+                evenCount++;
+            }
+
+            else{
+                oddCount++;
+            }
+        }
         input.close();
     }
 }
