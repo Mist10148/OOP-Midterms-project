fd24b621b91688fb5b33b49452185af08b611ca4
mist <74228226+Mist10148@users.noreply.github.com>
2026-09-11 17:14:45 +0800

    Add scanner initialization for user input in CashDenomination class

--- CashDenomination.java
+++ CashDenomination.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class CashDenomination {
     public static void main(String[] args){
+        
+
+        //making an object scanner for input
+        Scanner input = new Scanner(System.in);
+
         
     }
 }
