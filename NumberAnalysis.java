860eab6d8264449c610f94cae57e0106ff64049d
mist <74228226+Mist10148@users.noreply.github.com>
2026-09-11 17:52:11 +0800

    Add main method and scanner initialization in NumberAnalysis class

--- NumberAnalysis.java
+++ NumberAnalysis.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
 public class NumberAnalysis {
-    
+    public static void main(String[] args){
+
+        //making an object scanner for input
+        Scanner input = new Scanner(System.in);
+
+    }
 }
