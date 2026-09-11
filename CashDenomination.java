c88282cf74cc263ad43c7af4001e7297648987a8
mist <74228226+Mist10148@users.noreply.github.com>
2026-09-11 17:35:46 +0800

    Enhance CashDenomination class to display remaining amount and total bills after withdrawal

--- CashDenomination.java
+++ CashDenomination.java
@@ -6,6 +6,63 @@ public class CashDenomination {
         //making an object scanner for input
         Scanner input = new Scanner(System.in);
 
-        
+        //welcome message and getting input from the user
+        System.out.print("Enter amount: ");
+        int amount = input.nextInt();
+
+        //condition to check if the amount is valid or not
+        if (amount <= 0 || amount % 10 != 0){
+            System.out.println("Invalid amount.");
+        }
+
+        else {
+            //getting the number of bills for each denomination
+            int thousand = amount / 1000; //getting the php1000 bills
+            amount %= 1000;
+
+            int p500 = amount /500; //getting the php500 bills
+            amount %= 500;
+
+            int p200 = amount / 200; //getting the php200 bills
+            amount %= 200;
+
+            int p100 = amount / 100; //getting the php100 bills
+            amount %= 100;
+
+            int p50 = amount / 50; //getting the php50 bills
+            amount %= 50;
+
+            int p20 = amount / 20; //getting the php20 bills
+            amount %= 20;
+
+
+            //condition to check if the amount is not zero
+            if (amount != 0){
+                System.out.println("Withdrawal cannot be processed. ERROR!");
+        }
+
+            else{
+
+                //displaying the number of bills for each
+                System.out.println("P1000: " + thousand);
+                System.out.println("P500: " + p500);
+                System.out.println("P200: " + p200);
+                System.out.println("P100: " + p100);
+                System.out.println("P50: " + p50);
+                System.out.println("P20: " + p20);
+
+
+                //summing up the number of bills
+                int bills = thousand + p500 + p200 + p100 + p50 + p20;
+
+                //displaying the remmaining amount and the sum number of bills
+                System.out.println("\nRemaining amount: " + amount);
+                System.out.println("Total bills: " + bills);
+
+        }
+
+        }
+
+        input.close();
     }
 }
